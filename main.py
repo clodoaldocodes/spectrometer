e29c54819c755be0169cccccd374ea0f813e89dd
clodoaldocodes <cdsfj@hotmail.com>
2023-06-19 12:36:35 -0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import seabreeze.spectrometers as sb
+import numpy as np
+
+devices = sb.list_devices()
+print(devices)
 
 spec = sb.Spectrometer.from_serial_number()
-spec.integration_time_micros(2000)
+spec.integration_time_micros(20000)
 
-spec.wavelengths()
+# print(spec.wavelengths())
+# print(spec.intensities())
 
-spec.intensities()
+wavelengths = np.array(spec.wavelengths())
+intensities = np.array(spec.intensities())
+
+print(np.size(wavelengths))
+print('Min: ' + str(np.min(wavelengths)))
+print('Max: ' + str(np.max(wavelengths)))
+
+for i in range(np.size(wavelengths)):
+    print(str(wavelengths[i]) + ' - ' + str(intensities[i]))
